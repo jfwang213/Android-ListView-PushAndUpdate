75e5c263649c5dc318944fc93dbf128f03282d0c
Jianfei Wang <jfwang213@gmail.com>
2011-12-23 21:59:55 +0800

    First create View

---
+++ src/com/jfwang213/PullUpdateListViewDemo/PullUpdateListViewDemo.java
@@ -0,0 +1,13 @@
+package com.jfwang213.PullUpdateListViewDemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PullUpdateListViewDemo extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
